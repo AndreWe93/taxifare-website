bbf548f26b55e888ce5c53099984157b1c12536d
Andre Weisskopp <a.weisskopp@gmail.com>
2023-11-24 12:24:09 +0100

    first draft of website

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import requests
+from datetime import datetime
+
+'''
+# NY Taxi Fare Predictor (by AW)
+'''
+
+st.markdown('''
+Hello friend, please insert the required information to get a taxi fare prediction
+''')
+
+
+## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
+
+pickup_datetime = st.text_input("Pickup Date and Time")
+pickup_longitude = st.text_input("Pickup Longitude")
+pickup_latitude = st.text_input("Pickup Latitude")
+dropoff_longitude = st.text_input("Dropoff Longitude")
+dropoff_latitude = st.text_input("Dropoff Latitude")
+passenger_count = st.text_input("Passenger Count")
+
+
+if st.button("Predict Fare"):
+    url = 'https://taxifare.lewagon.ai/predict'
+
+    params = {
+            'pickup_datetime': pickup_datetime,
+            'pickup_longitude': pickup_longitude,
+            'pickup_latitude': pickup_latitude,
+            'dropoff_longitude': dropoff_longitude,
+            'dropoff_latitude': dropoff_latitude,
+            'passenger_count': passenger_count
+        }
+
+    # Make API request
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        # Retrieve prediction from JSON response
+        prediction = response.json()["fare"]
+
+        # Display prediction to the user
+        st.success(f'The predicted fare amount is: ${prediction:.2f}')
+    else:
+        st.error('Failed to get prediction from the API. Please try again.')
